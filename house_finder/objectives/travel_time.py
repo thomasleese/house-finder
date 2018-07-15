0f65b5fed85bd61888a6c68b04579725b4987b6e
thomas@leese.io <thomas@leese.io>
2018-07-15 13:40:31 +0100

    Moved common travel time attributes into base class

--- house_finder/objectives/travel_time.py
+++ house_finder/objectives/travel_time.py
@@ -7,20 +7,30 @@ from .objective import Objective
 
 class TravelTimeObjective(Objective):
 
+    def __init__(self, name, travel_time_calculator, mode, arrival_time=None, departure_time=None):
+        super().__init__(name)
+        self.travel_time_calculator = travel_time_calculator
+        self.mode = mode
+        self.arrival_time = arrival_time
+        self.departure_time = departure_time
+
     @staticmethod
     def from_dict(maps, travel_time_calculator, config):
-        return SingleTravelTimeObjective.from_dict(maps, travel_time_calculator, config)
+        if 'to_place_type' in config['params']:
+            return MultipleTravelTimeObjective.from_dict(
+                maps, travel_time_calculator, config
+            )
+        else:
+            return SingleTravelTimeObjective.from_dict(
+                maps, travel_time_calculator, config
+            )
 
 
 class SingleTravelTimeObjective(TravelTimeObjective):
 
     def __init__(self, travel_time_calculator, name, location, mode, arrival_time=None, departure_time=None):
-        super().__init__(name)
-        self.travel_time_calculator = travel_time_calculator
+        super().__init__(name, travel_time_calculator, mode, arrival_time, departure_time)
         self.location = location
-        self.mode = mode
-        self.arrival_time = arrival_time
-        self.departure_time = departure_time
 
     def calculate(self, listing):
         return self.travel_time_calculator(
@@ -44,4 +54,24 @@ class SingleTravelTimeObjective(TravelTimeObjective):
             travel_time_calculator, config['name'], lat_long,
             config['params']['via'], config['params'].get('arriving_at'),
             config['params'].get('leaving_at')
+        )
+
+
+class MultipleTravelTimeObjective(TravelTimeObjective):
+
+    def __init__(self, name, travel_time_calculator, maps, place_type, mode, arrival_time=None, departure_time=None):
+        super().__init__(name, travel_time_calculator, mode, arrival_time, departure_time)
+        self.maps = maps
+        self.place_type = place_type
+
+    def calculate(self, listing):
+        return 0
+
+    @classmethod
+    def from_dict(cls, maps, travel_time_calculator, config):
+        return cls(
+            config['name'], travel_time_calculator, maps,
+            config['params']['to_place_type'], config['params']['via'],
+            config['params'].get('arriving_at'),
+            config['params'].get('leaving_at'),
         )
